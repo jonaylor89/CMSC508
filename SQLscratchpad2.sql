f6edd4096792d2efe673a6addf9b58f3a016884e
John Naylor <jonaylor89@gmail.com>
2019-04-23 12:23:38 -0400

    gitignore

--- SQLscratchpad2.sql
+++ SQLscratchpad2.sql
@@ -3,7 +3,7 @@
 
 -- 1.)
 
-CREATE SEQUENCE seq_pk_employees
+CREATE OR REPLACE SEQUENCE seq_pk_employees
 MINVALUE 1
 MAXVALUE 100000000
 START WITH 1
@@ -12,10 +12,43 @@ NOCYCLE;
 
 -- 2.)
 
-CREATE FUNCTION ( employee_id )
+CREATE OR REPLACE FUNCTION ( employee_id )
 RETURN VARCHAR2(40)
 BEGIN
 
 END;
 
 /**************************************/
+
+CREATE OR REPLACE TRIGGER total_salary
+AFTER DELETE OR INSERT OR UPDATE department_id, salary, ON employees
+  FOR EACH ROW
+  BEGIN
+    IF DELETING OR (UPDATING AND :old.department_id != :new.department_id)
+
+    END IF;
+    IF INSERTING OR (UPDATING AND)
+
+    END IF;
+    IF (UPDATING AND)
+
+    END IF;
+  END
+END;
+
+CREATE OR REPLACE TRIGGER new_employee_bonus
+BEFORE INSERT ON employees
+FOR EACH ROW
+BEGIN
+    UPDATE EMPLOYEES
+    SET salary = salary * 1.05
+    WHERE department_id = :new.department_id
+END;
+
+
+CREATE TABLE 
+
+
+
+
+/************************************/
